f8721234777f05325e0ecc3391ca0eb0edf1987e
p0sd7 <0-locals-daring@icloud.com>
2026-06-13 05:20:07 +0300

    All works(client edition)

--- Code/src/LogicSystem/game.cpp
+++ Code/src/LogicSystem/game.cpp
@@ -12,7 +12,7 @@ namespace kernel {
 void Game::Run() {
   srand(static_cast<unsigned>(time(nullptr)));
   data_.LoadAll("../assets/");
-  int start_loc_id = data_.GetLocationIdByName("/root");
+  int start_loc_id = data_.GetLocationIdByName("/ash");
   if (start_loc_id == -1) start_loc_id = 1;
   auto spawn = data_.GetSpawnPoint(start_loc_id);
   auto player = std::make_unique<Entity>(0, EntityType::kPlayer, '@',
